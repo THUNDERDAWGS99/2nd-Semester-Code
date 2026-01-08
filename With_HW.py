8fe1d11045ee5e8bf7daf52f9276a7457c56f104
THUNDERDAWGS99 <13425@edu.wheatlandhigh.org>
2026-01-08 15:03:09 -0800

    Changed it to where it uses only 1 with open loop instead of 2

--- With_HW.py
+++ With_HW.py
@@ -1,18 +1,18 @@
 num_entries = int(input('How many entries do you want to enter? '))
 
-file_variable = open('steps_count.txt','w')
-for num in range(1, num_entries + 1):
-  steps = input(f'#{num} How many steps would you like to add to the file?  ')
-  file_variable.write(f'{steps}\n')
-file_variable.close()
+with open('steps_count.txt','w') as file_variable, open('steps_count.txt','r') as entry_list:
+  for num in range(1, num_entries + 1):
+    steps = input(f'#{num} How many steps would you like to add to the file? ')
+    file_variable.write(f'{steps}\n')
 
-entry_list = open('steps_count.txt','r')
-line = entry_list.readline()
-total_steps = 0
-
-while line != '': 
-  total_steps += int(line)
+  file_variable.flush()
+  entry_list.seek(0)
+  
   line = entry_list.readline()
-entry_list.close()
+  total_steps = 0
+
+  while line != '': 
+    total_steps += int(line)
+    line = entry_list.readline()
 
 print(f'The total steps of the numbers in the list is {total_steps}')
